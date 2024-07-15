99dc24b092a9be428a44498e0151b6d8fa8b6600
User <magdapaszko90@gmail.com>
2024-07-15 16:59:01 +0200

    add test page

---
+++ my-project-nabthat/vite.config.js
@@ -0,0 +1,18 @@
+import { defineConfig } from 'vite';
+import copy from 'rollup-plugin-copy';
+
+export default defineConfig({
+    build: {
+        outDir: './public/',
+        assetsDir: 'style',
+    },
+    plugins: [
+        copy({
+            targets: [
+                { src: 'src/images/*', dest: 'public/images' },
+                { src: 'src/json/*', dest: 'public/json' },
+            ],
+            hook: 'writeBundle', // Kopiuj pliki po zakończeniu procesu budowania
+        }),
+    ],
+});
