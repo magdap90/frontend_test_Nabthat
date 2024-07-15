164fcf48d33feff81b38c2ab6c31461625d13100
User <magdapaszko90@gmail.com>
2024-07-15 02:11:47 +0200

    add test page

--- my-project/src/js/main.js
+++ my-project/src/js/main.js
@@ -1,1 +1,21 @@
-console.log('xxxxxxxxxxxxxx');
+// let json = '{"descriptions":[' +
+//     '{"story" : "aa a aa aaa a"},' +
+//     '{"story" : "bb bbb bbbb bbbb bbb" },' +
+//     '{"story" : "cc ccc cccc cccc c"},' +
+//     '{"story" : "dd dd ddd ddd" },' +
+//     '{"story" : "ee eee eee eee" },' +
+//     '{"story" : "ff ffff ffffff ff"},' +
+//     '{"story" : "ggg gggg ggggg gg" }]}';
+//
+// const obj = JSON.parse(json);
+// let listDescriptions = obj.descriptions;
+// listDescriptions.forEach(el =>{
+//    console.log(el.story);
+// });
+// let list = document.querySelectorAll('input[name="size"]');
+// console.log(document.querySelectorAll('input[name="option"]'));
+// document.querySelectorAll('input[name="option"]').forEach((radio) => {
+//    radio.addEventListener('change', (event) => {
+//       console.log(`Selected option: ${event.target.value}`);
+//    });
+// });
